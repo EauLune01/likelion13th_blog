c16ad94e3a8755d0741b0b633d950463087ba7ea
최성민 <smsm7288@gmail.com>
2025-05-23 00:58:43 +0900

    Feat: JPA를 사용한 코드 리팩토링

--- src/main/java/likelion13th/likelionblog/repository/ArticleRepository.java
+++ src/main/java/likelion13th/likelionblog/repository/ArticleRepository.java
@@ -5,8 +5,13 @@ import likelion13th.likelionblog.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<Article,Long> {
     // JpaRepository 클래스 상속받음
     // 엔티티 Article과 기본키 타입을 인수로
+
+    // Article 제목 기반으로 객체 탐색
+    Optional<Article> findByTitle(String title);
 }
